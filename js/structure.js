7a1dd75915ba7b47bb321e6eca1754cce4b4597d
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2016-12-02 16:24:20 +0000

    The structure is now the CV

--- js/structure.js
+++ js/structure.js
@@ -1,3 +1,92 @@
 $(function (){
     console.log('Load');
 });
+
+$(document).ready(function(){
+  $('.html').mouseover(function(){
+    $('.desc-html').css('display','block');
+  $('.html').mouseout(function(){
+    $('.desc-html').css('display','none');
+  	});
+  });
+  
+  $('.css').mouseover(function(){
+    $('.desc-css').css('display','block');
+  $('.css').mouseout(function(){
+    $('.desc-css').css('display','none');
+  	});
+  });
+  
+  $('.js').mouseover(function(){
+    $('.desc-js').css('display','block');
+  $('.js').mouseout(function(){
+    $('.desc-js').css('display','none');
+  	});
+  });
+  
+  $('.jquery').mouseover(function(){
+    $('.desc-jquery').css('display','block');
+  $('.jquery').mouseout(function(){
+    $('.desc-jquery').css('display','none');
+  	});
+  });
+  
+  $('.php').mouseover(function(){
+    $('.desc-php').css('display','block');
+  $('.php').mouseout(function(){
+    $('.desc-php').css('display','none');
+  	});
+  });
+});
+
+//SKILLS BAR
+$(document).ready(function(){
+  $('.html').mouseover(function(){
+    $('.desc-html').css('display','block');
+  $('.html').mouseout(function(){
+    $('.desc-html').css('display','none');
+  	});
+  });
+  
+  $('.css').mouseover(function(){
+    $('.desc-css').css('display','block');
+  $('.css').mouseout(function(){
+    $('.desc-css').css('display','none');
+  	});
+  });
+  
+  $('.js').mouseover(function(){
+    $('.desc-js').css('display','block');
+  $('.js').mouseout(function(){
+    $('.desc-js').css('display','none');
+  	});
+  });
+  
+  $('.jquery').mouseover(function(){
+    $('.desc-jquery').css('display','block');
+  $('.jquery').mouseout(function(){
+    $('.desc-jquery').css('display','none');
+  	});
+  });
+  
+  $('.php').mouseover(function(){
+    $('.desc-php').css('display','block');
+  $('.php').mouseout(function(){
+    $('.desc-php').css('display','none');
+  	});
+  });
+
+$('.java').mouseover(function(){
+    $('.desc-java').css('display','block');
+  $('.java').mouseout(function(){
+    $('.desc-java').css('display','none');
+  	});
+  });
+
+$('.git').mouseover(function(){
+    $('.desc-git').css('display','block');
+  $('.git').mouseout(function(){
+    $('.desc-git').css('display','none');
+  	});
+  });
+});
